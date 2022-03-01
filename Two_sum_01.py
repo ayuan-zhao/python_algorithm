f462c436c1725934838ffd25482865216ed45f51
Ayuan Zhao <ayuan@Ayuans-Mac.local>
2022-03-01 15:41:25 -0800

    twosum

---
+++ Two_sum_01.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+# You can return the answer in any order.
+
+# 由于一个key只能对应一个value，所以，多次对一个key放入value，后面的值会把前面的值冲掉：
+# dict可以用在需要高速查找的很多地方，在Python代码中几乎无处不在，正确使用dict非常重要，需要牢记的第一条就是dict的key必须是不可变对象。
+# 字符串、整数等都是不可变的，因此，可以放心地作为key，而list是可变的，就不能作为key：
+
+
+class Solution:
+    # 暴力解
+    def twoSumBoLi(self, nums, target):
+        for i in nums:
+            # 因为两数相加等于target,所以先循环一个，另一个是target - i
+            j = target - i
+            # 取这个数组的index nums.index(i)
+            start_index = nums.index(i)
+            next_index = start_index + 1
+            # 重新建了一个array,nums[next_index:]打到最后
+            temp_nums = nums[next_index:]
+            if j in temp_nums:
+                # 如果就是下一个，就是next_index + temp_nums.index(j), 只不过这时，temp_nums.index(j) = 0
+                return(nums.index(i), next_index + temp_nums.index(j))
+
+    def twoSum(self, nums, target):
+        # 建一个新的dictionary
+        dict = {}
+        for i in range(len(nums)):
+            #一个数是否 in dict,这是在查找key是不是存在
+            if target - nums[i] not in dict:
+                # nums[i]是key,nums里具体的数是key,indexing是value,每次把扫到的数存到dict里,然后检查，target -目前扫到的数在不在dic里
+                # 比如寻找15，就15-2，看看有没有13，没有就dict[2:0],然后15-5，看看有没有10，没有就存dict[2:0,5:1],然后15-10，看看有没有5
+                # 发现dict中的key有5，然后就返回
+                # 当我们用target- nums[i],得到的那个值在 dic
+                dict[nums[i]] = i
+            else:
+                # dict[target-nums[i]]
+                return [dict[target - nums[i]], i]
+
+    def twoSumOpt(self, nums, target):
+        prevmap = {}  # val: index
+        # 这里的i是indexing,n是value
+        for i, n in enumerate(nums):
+            #print("i", i)
+            #print("n", n)
+            # 找的是这个key
+            if target - n in prevmap:
+                print ( "i=", i, n=", n, "target", target, "target - n= ", target - n)
+                print("prevmap[target - n]", prevmap[target - n])
+                return [prevmap[target - n], i]
+
+            prevmap[n] = i
+            print(prevmap)
+        return
+
+
+so = Solution()
+nums = [1, 3, 4, 5, 6, 10, 7]
+ts = so.twoSum(nums, 15)
+print(ts)
+
+so = Solution()
+nums = [1, 3, 4, 5, 6, 10, 7]
+# prevmap 最后的样子{1: 0, 3: 1, 4: 2, 5: 3, 6: 4}
+ts = so.twoSumOpt(nums, 15)
+print(ts)
+# 4 - 1 = 3, we just check if 3 is exist
+# use hash map
+# maping the value to the index
+# 为什么要建一个新的hash map而不是直接把list放入hash map,直接去查
+# 因为，每个元素不能用两遍，比如target是4， 4-2 = 2,2已经在里面了
+
+# it can be initially empty
+# iterate through the array   O（N）
+# adding each value to our hash map————constant time
+# checking if a value exists in our hash ——————constant time
--- practice.py
+++ practice.py
@@ -3,21 +3,26 @@ import numpy
 from numpy import matrix
 
 class Solution:
-    def search(self,nums,target):
-        left,right = 0,len(nums)-1
-        while left <= right:
-            middle = left + (right-left)//2
-            if nums[middle] < target:
-                left = middle + 1
-            elif nums[middle] > target:
-                right = middle- 1
-            else :
-                return middle
-        return -1               
-         
-so = Solution()
-nums1= [1,2,4,5,6,7,8]
-res = so.search(nums1,2)
-print(res)
+   def minSubArrayLen(self,nums,totaltarget):
+    res = float("inf")
+    total = 0
+    left = 0
+    for right in  range(len(nums)):
+        total += nums[right]
+        while total >= totaltarget:
+            res = min(res,right - left + 1 )
+            total = total - nums[left]
+            left += 1
+    return 0 if res == float("inf") else res       
 
+
+ 
+
+
+
+
+so = Solution()
+nums1 = [1, 2, 4, 5, 2, 1]
+answer = so.minSubArrayLen(nums1, 13 )
+print(answer)
 
